5767bb29f6f912ece2d355d807c8c520e027c140
Replit user <>
2023-09-14 18:06:15 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -88,9 +88,9 @@ TreeNode * minimum(TreeNode * x){
     return NULL;
   }
 
-  TreeNode* aux = x->left;
+  TreeNode* aux = x->right;
   while(aux!=NULL){
-    aux=aux->left;
+    aux=aux->right;
   }
   return x;
 }
